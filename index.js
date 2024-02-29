e87f22455627d10b5b1ec5705857667b4d31d9be
Terri Fricker <51532684+terrifricker@users.noreply.github.com>
2024-02-29 08:58:24 -0800

    add middleware express.static('dis')

--- index.js
+++ index.js
@@ -9,6 +9,7 @@ const morganFormat = ':method :url :status :res[content-length] - :response-time
 
 app.use(cors())
 app.use(express.json())
+app.use(express.static('dist'))
 app.use(morgan(morganFormat))
 
 let persons = [
